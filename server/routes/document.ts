5296e280404e4136d663ba16b671826f58dc1aa1
Joep Buhre <jcbuhre@gmail.com>
2023-04-22 00:38:46 +0200

    checked linting

--- server/routes/document.ts
+++ server/routes/document.ts
@@ -6,18 +6,14 @@ const mult = multer({ storage: multer.memoryStorage() });
 
 const document = Router();
 
-document.use(
-    "/create",
-    mult.array("document"),
-    (req: Request, res: Response) => {
-        if (req.files) {
-            handleSaveDocument(req.files);
-            res.status(201).send("OK");
-        } else {
-            res.status(400).send("No files added");
-            logger.error("No files added");
-        }
+document.use("/create", mult.array("document"), (req: Request, res: Response) => {
+    if (req.files) {
+        handleSaveDocument(req.files);
+        res.status(201).send("OK");
+    } else {
+        res.status(400).send("No files added");
+        logger.error("No files added");
     }
-);
+});
 
 export default document;
